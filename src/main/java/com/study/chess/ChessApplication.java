d238b58196ea03661853c8647294a1dc86bb2296
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-27 01:32:56 -0300

    FEAT: main using UI

--- src/main/java/com/study/chess/ChessApplication.java
+++ src/main/java/com/study/chess/ChessApplication.java
@@ -1,4 +1,14 @@
 package com.study.chess;
 
+
+import com.study.chess.core.chess.ChessMatch;
+
 public class ChessApplication {
+
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
+    }
 }
