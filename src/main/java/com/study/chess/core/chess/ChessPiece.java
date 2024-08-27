f2f04794fc3b684be77be43a9ed4c7dd7226cc9d
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-27 01:30:50 -0300

    FEAT: initial chess game structure

--- src/main/java/com/study/chess/core/chess/ChessPiece.java
+++ src/main/java/com/study/chess/core/chess/ChessPiece.java
@@ -1,2 +1,18 @@
-package com.study.chess.core.chess;public class ChessPiece {
+package com.study.chess.core.chess;
+
+import com.study.chess.core.boardgame.Board;
+import com.study.chess.core.boardgame.Piece;
+
+public class ChessPiece extends Piece {
+
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
--- src/main/java/com/study/chess/core/chess/Color.java
+++ src/main/java/com/study/chess/core/chess/Color.java
@@ -1,2 +1,10 @@
-package com.study.chess.core.chess;public enum Color {
+package com.study.chess.core.chess;
+
+public enum Color {
+    BLACK("Preto"),
+    WHITE("Branco");
+
+    private final String color;
+
+    Color(String color){this.color = color;}
 }
