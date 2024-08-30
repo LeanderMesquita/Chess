53c8fa1ef9ef596398d6ec5edb392595d425c512
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-30 15:50:40 -0300

    FEAT: chess classes

--- src/main/java/com/study/chess/core/chess/pieces/King.java
+++ src/main/java/com/study/chess/core/chess/pieces/King.java
@@ -1,2 +1,16 @@
-package com.study.chess.core.chess.pieces;public class King {
+package com.study.chess.core.chess.pieces;
+
+import com.study.chess.core.boardgame.Board;
+import com.study.chess.core.chess.ChessPiece;
+import com.study.chess.core.chess.Color;
+
+public class King extends ChessPiece {
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString(){
+        return "K";
+    }
 }
--- src/main/java/com/study/chess/core/chess/pieces/Rook.java
+++ src/main/java/com/study/chess/core/chess/pieces/Rook.java
@@ -1,2 +1,17 @@
-package com.study.chess.core.chess.pieces;public class Rook {
+package com.study.chess.core.chess.pieces;
+
+import com.study.chess.core.boardgame.Board;
+import com.study.chess.core.chess.ChessPiece;
+import com.study.chess.core.chess.Color;
+
+public class Rook extends ChessPiece {
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
 }
