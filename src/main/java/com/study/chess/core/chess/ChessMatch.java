cb519adccf905f7c2fade138c42ba9a0b47963aa
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-30 15:53:46 -0300

    WIP: implementing chess match initial setup

--- src/main/java/com/study/chess/core/chess/ChessMatch.java
+++ src/main/java/com/study/chess/core/chess/ChessMatch.java
@@ -1,13 +1,16 @@
 package com.study.chess.core.chess;
 
 import com.study.chess.core.boardgame.Board;
+import com.study.chess.core.chess.pieces.King;
+import com.study.chess.core.chess.pieces.Rook;
 
 public class ChessMatch {
 
-    private Board board;
+    private final Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
@@ -21,5 +24,25 @@ public class ChessMatch {
         }
 
         return mat;
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    private void initialSetup(){
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
