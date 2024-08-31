a5fd6698e8b4597ac5a22f04afa4bd0322bebee6
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-31 14:48:59 -0300

    FEAT: exceptions treatment and improving UI

--- src/main/java/com/study/chess/ChessApplication.java
+++ src/main/java/com/study/chess/ChessApplication.java
@@ -2,13 +2,40 @@ package com.study.chess;
 
 
 import com.study.chess.core.chess.ChessMatch;
+import com.study.chess.core.chess.ChessPiece;
+import com.study.chess.core.chess.ChessPosition;
+import com.study.chess.core.exceptions.ChessException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ChessApplication {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true){
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performeChessMove(source, target);
+            }catch (ChessException | InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
+
 
     }
 }
