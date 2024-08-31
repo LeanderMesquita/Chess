39e2f395b7f5fd1a2cd7b1d747046cc554c3514f
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-31 15:48:37 -0300

    FEAT: abstract implementation of piece and overriding abstract method

--- src/main/java/com/study/chess/core/chess/ChessPiece.java
+++ src/main/java/com/study/chess/core/chess/ChessPiece.java
@@ -3,9 +3,9 @@ package com.study.chess.core.chess;
 import com.study.chess.core.boardgame.Board;
 import com.study.chess.core.boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
-    private Color color;
+    private final Color color;
 
     public ChessPiece(Board board, Color color) {
         super(board);
